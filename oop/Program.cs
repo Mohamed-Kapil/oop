b5c171773962767acd15c960dc3a83795f49c223
kapil <mohamedkapil31@gmail.com>
2026-09-20 11:10:37 +0300

    question 1 (a)

--- oop/Program.cs
+++ oop/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-          
+            #region question 1 (a)
+            /*When a DeliveryAddress variable is copied, a new independent copy of the value is created.
+             Modifying the copied variable does not affect the original variable.*/
+
+            #endregion
         }
     }
 }
