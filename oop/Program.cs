008a9c6d814bde11fb8f42ab3f50c4b6fdf73df1
kapil <mohamedkapil31@gmail.com>
2026-09-20 21:31:07 +0300

    question part 2

--- oop/Program.cs
+++ oop/Program.cs
@@ -27,6 +27,92 @@ namespace oop
             /* Private fields and public properties can improve encapsulation by hiding the internal data and controlling access to it.
                The properties can use get and set accessors, and validation can be added inside the set accessor.*/
             #endregion
+
+            #region question part 2 
+            DeliveryCenter center = new DeliveryCenter();
+
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"Enter Shipment {i} Data");
+
+                Console.Write("Tracking Code: ");
+                string trackingCode = Console.ReadLine();
+
+                Console.Write("Description: ");
+                string description = Console.ReadLine();
+
+                Console.Write("Weight: ");
+                double weight = double.Parse(Console.ReadLine());
+
+                Console.Write("Delivery Fee: ");
+                decimal deliveryFee = decimal.Parse(Console.ReadLine());
+
+                Console.Write("City: ");
+                string city = Console.ReadLine();
+
+                Console.Write("Street: ");
+                string street = Console.ReadLine();
+
+                Console.Write("Building Number: ");
+                int buildingNumber = int.Parse(Console.ReadLine());
+
+                DeliveryAddress address = new DeliveryAddress(city, street, buildingNumber);
+
+                Shipment shipment = new Shipment(trackingCode, description, weight, deliveryFee, address);
+
+                if (center.AddShipment(shipment))
+                {
+                    Console.WriteLine("Shipment added successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Delivery center is full.");
+                }
+
+            }
+            Console.WriteLine("--- All Shipments ---");
+
+            for (int i = 0; i < 3; i++)
+            {
+                center[i].PrintShipment();
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter a tracking code to search: ");
+            string searchCode = Console.ReadLine();
+
+            Shipment foundShipment = center[searchCode];
+
+            if (!string.IsNullOrEmpty(foundShipment.TrackingCode))
+            {
+                Console.WriteLine(
+                    $"Shipment found: {foundShipment.TrackingCode} - {foundShipment.Description}"
+                );
+            }
+            else
+            {
+                Console.WriteLine("Shipment not found.");
+            }
+            Console.WriteLine();
+            Console.WriteLine("--- Struct Copy Test ---");
+
+            DeliveryAddress originalAddress =
+                new DeliveryAddress(
+                    "Cairo",
+                    "Tahrir Street",
+                    15
+                );
+
+            DeliveryAddress copiedAddress = originalAddress;
+
+            copiedAddress.Street = "Makram Ebeid Street";
+            copiedAddress.BuildingNumber = 20;
+
+            Console.WriteLine(
+                "Original Address: " + originalAddress.GetFullAddress());
+
+            Console.WriteLine("Copied Address: " + copiedAddress.GetFullAddress());
+            #endregion
         }
     }
 }
---
+++ oop/Shipment.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop
+{
+    public struct Shipment
+    {
+       
+        
+            private string trackingCode;
+            private string description;
+            private double weight;
+            private decimal deliveryFee;
+
+            public DeliveryAddress Destination { get; set; }
+
+            public string TrackingCode
+            {
+                get
+                {
+                    return trackingCode;
+                }
+            }
+
+            public string Description
+            {
+                get
+                {
+                    return description;
+                }
+
+                set
+                {
+                    if (!string.IsNullOrWhiteSpace(value))
+                    {
+                        description = value;
+                    }
+                }
+            }
+            public decimal DeliveryFee
+            {
+                get
+                {
+                    return deliveryFee;
+                }
+
+                private set
+                {
+                    if (value > 0)
+                    {
+                        deliveryFee = value;
+                    }
+                }
+            }
+
+            public decimal EstimatedCost
+            {
+                get
+                {
+                    return DeliveryFee + ((decimal)weight * 5);
+                }
+            }
+            public Shipment(string trackingCode)
+            {
+                this.trackingCode = trackingCode;
+                description = "Unknown";
+                weight = 1;
+                deliveryFee = 50;
+                Destination =
+                    new DeliveryAddress("Unknown", "Unknown", 0);
+            }
+
+            public Shipment(
+                string trackingCode,
+                string description,
+                double weight,
+                decimal deliveryFee,
+                DeliveryAddress destination)
+            {
+                this.trackingCode = trackingCode;
+
+                this.description =
+                    !string.IsNullOrWhiteSpace(description)
+                    ? description
+                    : "Unknown";
+
+                this.weight =
+                    weight > 0
+                    ? weight
+                    : 1;
+                this.deliveryFee = deliveryFee > 0 ? deliveryFee : 50;
+
+                Destination = destination;
+            }
+              public void UpdateDeliveryFee(decimal newFee)
+               {
+                 if (newFee > 0)
+                  {
+                   deliveryFee = newFee;
+                  }
+               }
+        public void PrintShipment()
+        {
+            Console.WriteLine($"Tracking Code: {TrackingCode}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Weight: {weight} KG");
+            Console.WriteLine($"Delivery Fee: {DeliveryFee} EGP");
+            Console.WriteLine($"Destination: {Destination.GetFullAddress()}");
+            Console.WriteLine($"Estimated Cost: {EstimatedCost} EGP");
+        }
+    }
+
+}
+ 
+    
+
