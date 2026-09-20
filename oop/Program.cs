08d84450d0b4827f51d37caed740c5541f88b3a3
kapil <mohamedkapil31@gmail.com>
2026-09-20 11:12:56 +0300

    question 1 (b)

--- oop/Program.cs
+++ oop/Program.cs
@@ -7,7 +7,11 @@
             #region question 1 (a)
             /*When a DeliveryAddress variable is copied, a new independent copy of the value is created.
              Modifying the copied variable does not affect the original variable.*/
+            #endregion
 
+            #region question 1 (b)
+            /*When a Customer variable is copied, both variables refer to the same object. Therefore, 
+             modifying the object through one variable also affects the other variable.*/
             #endregion
         }
     }
