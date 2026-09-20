c44a9fe4944c39c5c76905747c483e09a1aa854d
kapil <mohamedkapil31@gmail.com>
2026-09-20 11:20:52 +0300

    question 2 (a)

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace oop
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace oop
 {
     internal class Program
     {
@@ -12,6 +14,13 @@
             #region question 1 (b)
             /*When a Customer variable is copied, both variables refer to the same object. Therefore, 
              modifying the object through one variable also affects the other variable.*/
+            #endregion
+
+            #region question 2 (a)
+         /* a) Problems with the design
+              1-The fields are public, so they can be accessed and modified directly from outside the class or struct.
+              2-There is no validation for the data.
+              3-There is no control over how the data is accessed or modified, which weakens encapsulation.*/
             #endregion
         }
     }
