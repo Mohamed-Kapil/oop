74d02570ffb6cb15d660ca6b8f0452a102e0ee67
kapil <mohamedkapil31@gmail.com>
2026-09-25 20:15:07 +0300

    part1 question 1 (a)

--- oop3/Program.cs
+++ oop3/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace oop3
+﻿using System.Numerics;
+
+namespace oop3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-          
+            #region part1 question 1 (a)
+            /* Method Overloading occurs when a class has multiple methods with the same name but different parameters.
+             * It is resolved at compile time and does not require inheritance.
+             *Method Overriding occurs when a derived class provides a new implementation for a method inherited from a base class.
+             *The method in the base class must be virtual or abstract, and the derived class uses override.
+             *It is resolved at runtime and requires inheritance.*/
+            #endregion
+            #region
+            #endregion
         }
     }
 }
