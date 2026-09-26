459eb029c7a3d4d934be84b4b50a181e03bd0419
kapil <mohamedkapil31@gmail.com>
2026-09-26 20:29:34 +0300

    part 1 question 1 (b)

--- oop4/Program.cs
+++ oop4/Program.cs
@@ -8,7 +8,11 @@
             /*Abstraction is the process of hiding complex implementation details and showing only the essential features of an object.*/
             #endregion
 
-            #region
+            #region part 1 question 1 (b)
+            /*Abstraction is one of the four pillars of OOP because it reduces complexity, hides unnecessary implementation details, and allows users to focus on what an object does rather than how it works.*/
+            #endregion
+
+            #region part 1 question 2 (a)
             #endregion
         }
     }
