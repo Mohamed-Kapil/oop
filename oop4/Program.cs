e3a8baa5d83b42602c327616599fe12774fede4b
kapil <mohamedkapil31@gmail.com>
2026-09-26 20:25:37 +0300

    part1 question1 (a)

---
+++ oop4/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace oop4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region part 1 question 1 (a)
+            /*Abstraction is the process of hiding complex implementation details and showing only the essential features of an object.*/
+            #endregion
+
+            #region
+            #endregion
+        }
+    }
+}
